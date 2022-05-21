5bde276f2eb6d58e982d8d9e1cccabf7ce8aca5b
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-05-21 21:01:49 +0300

    init

--- ReverseWords/Controllers/ViewController.swift
+++ ReverseWords/Controllers/ViewController.swift
@@ -1,10 +1,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var reverseButton: ReverseButton!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var reverseTextField: ReverseTextField!
-    @IBOutlet weak var lineView: LineView!
     @IBOutlet weak var navBar: UINavigationBar! {
         didSet {
             navBar.hideNavBarLine()
@@ -17,31 +15,12 @@ class ViewController: UIViewController {
         reverseTextField.delegate = self
     }
 
-    @IBAction func reverseAction(_ sender: UIButton) {
-        if reverseButton.isReversed {
-            reverseButton.isReversed = false
-            resultLabel.isHidden = true
-            reverseTextField.text = ""
-            reverseTextField.isActive = true
-            lineView.isActive = true
-            reverseButton.disableButton()
-        } else {
-            resultLabel.text = ReversePhrase.reverse(
-                phrase: reverseTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines))
-            resultLabel.isHidden = false
-            reverseTextField.isActive = false
-            lineView.isActive = false
-            reverseButton.isReversed = true
-        }
-    }
-
     @IBAction func textFildEditingChanged(_ sender: UITextField) {
         reverseTextField.isActive = true
-        lineView.isActive = true
         if sender.text?.count ?? 0 > 0 {
-            reverseButton.enableButton()
+//            reverseButton.enableButton()
         } else {
-            reverseButton.disableButton()
+//            reverseButton.disableButton()
         }
     }
 
